090ac50b99b6e6816181201c3a5dee95bcafe66c
seasons986 <annalin12345@gmail.com>
2023-09-01 18:52:13 -0400

    forgot i didn't know how to convert simplified to traditional in JS yet

--- index.js
+++ index.js
@@ -12,19 +12,37 @@ window.addEventListener("load", () => {
   let rowIndex = 0;
 
   document.getElementById("reveal").onclick = (e) => {
-    if (!solution) return;
+    console.log({entry})
+    if (!solution || entry.join("").length != 4) return;
     e.target.innerHTML = "⚉";
     e.target.style.color = "#f9faffde";
-    document.getElementById(`row-${row}`);
-    console.log({solution, entry})
-    // validate(col, td.innerText);
+    validate();
   }
 
-  function validate(col, ch) {
-    console.log({col, ch});
+  // pinyin only!!! soz T_T
+  function validate() {
     console.log({entry, solution});
+    const sol = solution.split("");
+    entry.forEach((ch, index) => {
+      const cell = document.getElementById(`input-${rowIndex}-${index}`);
+      console.log({cell})
+      // first check if in correct spot
+      if (ch == sol[index]) cell.parentElement.className = "green";
+      // then check correct word in wrong spot
+      else if (solution.includes(ch)) cell.parentElement.className = "yellow";
+      else cell.parentElement.className = "gray"
+      cell.readOnly = true;
+    });
     rowIndex++;
+    if (rowIndex < 6) document.getElementById(`input-${rowIndex}-0`).focus();
     if (rowIndex > 6) alert("failed");
+  }
+
+  function onType(input, val, r, c) {
+    if (!val || !val.match(/^.*[一-龥]/g)) return;
+    if (val.length > 1) val = input.value = val.trim().split("")[0];
+    if (c < 3) document.getElementById(`input-${r}-${c+1}`).focus(); // auto-tab to next input element in row
+    entry[c] = val;
   }
 
   function initGrid() {
@@ -35,12 +53,9 @@ window.addEventListener("load", () => {
         const td = document.createElement("td");
         const input = document.createElement("input");
         input.type = "text";
-        input.id = `td-${row}-${col}'`;
+        input.id = `input-${row}-${col}`;
         td.appendChild(input);
-        input.onchange = (ev) => {
-          entry[col] = ev.target.value;
-          console.log({ev, entry});
-        }
+        input.oninput = (ev) => onType(ev.target, ev.target.value, row, col);
         tr.appendChild(td);
       }
       grid.appendChild(tr);
@@ -50,7 +65,9 @@ window.addEventListener("load", () => {
   // amazing chengyu data source -- http://thuocl.thunlp.org/
   fetch("THUOCL_chengyu.txt").then((f) => f.text()).then((r) => {
     phrases = r.split(",");
-    solution = phrases[getRandomInt(0, phrases.length)];
+    if (!phrases) return;
+    while (!solution || solution.length != 4) solution = phrases[getRandomInt(0, phrases.length)];
+    console.log(solution);
     initGrid();
   });
 });
